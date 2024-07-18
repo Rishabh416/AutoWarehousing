a73b32e4bbf38e2e8cf36c67d3258afa83c97c39
rishabh416 <mittalr2008@gmail.com>
2024-07-18 10:04:42 +0400

    temp code storage

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from barcode.writer import ImageWriter
 from pyzbar.pyzbar import decode
 import cv2
 
+import time
+
 code128 = barcode.get_barcode_class("code128")
 
 def generateBarcode(data):
@@ -12,15 +14,31 @@ def generateBarcode(data):
 
 MODE = "SCAN"
 
+storedCodes = []
+
 if MODE == "GENERATE":
     generateBarcode("location 04 02")
 else:
     vid = cv2.VideoCapture(0)
     while(True): 
         ret, frame = vid.read()
+        cv2.imshow("scanner", frame)
         scanned = decode(frame)
         try:
-            print(scanned[0].data.decode('utf-8'))
+            scannedCode = scanned[0].data.decode('utf-8')
+            if not(scannedCode in storedCodes):
+                print(scannedCode)
+                storedCodes.append(scannedCode)
         except:
             print("No Barcode") 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+vid.release()
+cv2.destroyAllWindows()
+
+for code in storedCodes:
+    if "location" in code:
+        print("location")
+    elif "product" in code:
+        print("product")
